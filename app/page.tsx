2944c65655992c0fe8b0964caf4d86580c0360c0
TAMILSELVAM.A <113787063+A-TAMILSELVAM08@users.noreply.github.com>
2026-03-24 11:58:30 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import bot from "../../public/BOT.png";
+import bot from "../public/BOT.png";
 import { useRouter } from "next/navigation";
 
 export default function Homepage() {
